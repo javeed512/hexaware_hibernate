6a4317d0eb752fb183b008ed62a19d5e1b4b8b6e
javeed512 <javeedmohammedjaveed@gmail.com>
2023-11-01 17:18:39 +0530

    Hibernate CRUD Operation , Queries Demos, PPTs,Notes

--- hibernate/src/main/java/com/hexaware/hibernate/entity/Employee.java
+++ hibernate/src/main/java/com/hexaware/hibernate/entity/Employee.java
@@ -2,14 +2,25 @@ package com.hexaware.hibernate.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="employee_table")
+
+
+@NamedQueries({
+	 @NamedQuery(name="getEmpRecord",query = "select e from Employee e") ,
+	 @NamedQuery(name="selectByName",query = "select e from Employee e where e.ename = :name ") 
+})
 public class Employee {
 	
 		@Id // PK
+		//@GeneratedValue(strategy = GenerationType.AUTO)
 		private int eid;
 		@Column(name="emp_name")
 		private String ename;
